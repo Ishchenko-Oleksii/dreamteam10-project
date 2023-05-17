a9542570b066da9863dfed6248932d0e4f2886d6
levchsergey86 <116516113+levchsergey86@users.noreply.github.com>
2023-05-17 21:34:27 +0300

    Revert "before pagination"
    This reverts commit d8041f6a4b44ab069ee1cd2fc1b6b0c4932469d4.

--- src/js/shopinglist.js
+++ src/js/shopinglist.js
@@ -1,16 +1,9 @@
 const books = JSON.parse(localStorage.getItem('info-shopping-list')) || [];
-import imageAmazon from '../images/amazon.svg';
-import imageBookShop from '../images/bookshop.svg';
-import imageIBooks from '../images/ibooks.svg';
-import deleteButton from '../images/sprite.svg#icon-trash';
-import EmptyPicture from '../images/emptyshopinglist.jpg';
-import 'tui-pagination/dist/tui-pagination.css';
-import Pagination from 'tui-pagination';
 
 console.log(books);
 
 function deleteBook(bookId) {
-  const index = books.findIndex(book => book.bookId === bookId);
+  const index = books.findIndex(book => book.id === bookId);
   if (index !== -1) {
     books.splice(index, 1);
     localStorage.setItem('info-shopping-list', JSON.stringify(books));
@@ -22,8 +15,8 @@ function createMarkup(books) {
   if (books.length === 0) {
     return `
       <div class="shoping-list__empty-page">
-        <p class="shoping-list__empty-text">This page is empty, add some books and proceed to order.</p>
-        <img class="shoping-list__empty-img" src=${EmptyPicture} alt="Empty page">
+        <p class="shoping-list__empty-text">Эта страница пуста, добавьте книги и перейдите к оформлению заказа.</p>
+        <img class="shoping-list__empty-img" src="./src/images/emptyshopinglist.jpg" alt="Пустая полка">
       </div>
     `;
   }
@@ -31,7 +24,7 @@ function createMarkup(books) {
   return books
     .map(
       ({
-        bookId,
+        id,
         bookTitle,
         bookImage,
         bookAuthor,
@@ -50,15 +43,15 @@ function createMarkup(books) {
             if (buyLink === bookAmazon) {
               iconWidth = '32px';
               iconHeight = '11px';
-              iconHref = imageAmazon;
+              iconHref = './images/amazon.svg';
             } else if (buyLink === bookOpenBook) {
               iconWidth = '16px';
               iconHeight = '16px';
-              iconHref = imageIBooks;
+              iconHref = './images/openbook.svg';
             } else if (buyLink === bookBookShop) {
               iconWidth = '16px';
               iconHeight = '16px';
-              iconHref = imageBookShop;
+              iconHref = './images/booksshop.svg';
             }
 
             linksHtml.push(`
@@ -82,9 +75,9 @@ function createMarkup(books) {
                   <h3 class="shoping-list__name">${bookTitle}</h3>
                   <p class="shoping-list__bind">${bookCategory}</p>
                 </div>
-                <button class="button_trash" data-book-id="${bookId}">
+                <button class="button_trash" data-book-id="${id}">
                   <svg width="18" height="18" class="icon_trash">
-                    <use href=${deleteButton}></use>
+                    <use href="./images/sprite.svg#icon-trash"></use>
                   </svg>
                 </button>
               </div>
