87b9da2164ef78f8510c753a36a694d4e6e5f0e1
Klymuk <114335106+Klymykk@users.noreply.github.com>
2023-05-17 19:30:20 +0300

    header

--- src/js/header.js
+++ src/js/header.js
@@ -1,55 +1,57 @@
-const openMenuBtn = document.querySelector('.open-menu-icon');
-const closeMenuBtn = document.querySelector(".close-menu-icon");
-const burgerMenu = document.querySelector('.burger-menu');
+const openMenuBtn = document.querySelector('.js-open-menu');
+const closeMenuBtn = document.querySelector(".js-close-menu");
+const burgerMenu = document.querySelector('.js-burger-menu');
 
-const customerLoggedIn = `<div class="burger-conteiner">
-  <div class="user-burger">
-    <div class="avatar">
-      <img class="avatar_img" src="" alt="">
-    </div>
-    <p class="user-name">Stephen</p>
-  </div>
+// const customerLoggedIn = `<div class="burger-conteiner">
+//   <div class="user-burger">
+//     <div class="avatar">
+//       <img class="avatar_img" src="" alt="">
+//     </div>
+//     <p class="user-name">Stephen</p>
+//   </div>
 
-  <nav class="navigation-burger">
-    <ul class="navigation-burger-list">
-      <li class="navigation-item navigation-burger-item"><a class="navigation-link" href="#">Home</a></li>
-      <li class="navigation-item navigation-burger-item">
-        <a class="navigation-link" href="#">Shopping List
-          <svg width="20" height="20">
-            <use href="./images/icons.svg#icon-lock"></use>
-          </svg>
-        </a>
-      </li>
-    </ul>
-  </nav>
+//   <nav class="navigation-burger">
+//     <ul class="navigation-burger-list">
+//       <li class="navigation-item navigation-burger-item"><a class="navigation-link" href="#">Home</a></li>
+//       <li class="navigation-item navigation-burger-item">
+//         <a class="navigation-link" href="#">Shopping List
+//           <svg width="20" height="20">
+//             <use href="/images/icons.svg#icon-lock"></use>
+//           </svg>
+//         </a>
+//       </li>
+//     </ul>
+//   </nav>
 
-  <button class="button_logout btn-logout-burger">Log out
-    <svg width="20" height="20">
-      <use href="./images/icons.svg#icon-arrow-narrow-right"></use>
-    </svg>
-  </button>
-</div>`;
+//   <button class="button_logout btn-logout-burger js-signout submit-out" type="button" value="">Log out
+//     <svg width="20" height="20">
+//       <use href="/images/icons.svg#icon-arrow-narrow-right"></use>
+//     </svg>
+//   </button>
+// </div>`;
 
-const customerNouLoggedIn = `<div class="burger-conteiner">
-  <button class="button_signup">Sign up
-    <svg width="20" height="20">
-      <use href="./images/icons.svg#icon-arrow-narrow-right"></use>
-    </svg>
-  </button>
-</div>`;
+// const customerNouLoggedIn = `<div class="burger-conteiner">
+//   <button class="button_signup btn-signup-burger js-signup-btn" type="button" value="" data-modal-open>Sign up
+//     <svg width="20" height="20">
+//       <use href="/images/icons.svg#icon-arrow-narrow-right"></use>
+//     </svg>
+//   </button>
+// </div>`;
 
 openMenuBtn.addEventListener('click', openMenu);
 closeMenuBtn.addEventListener('click', closeMenu);
 
 function openMenu() {
-  burgerMenu.classList.remove("is-hidden");
+  // burgerMenu.classList.remove("is-hidden");
   openMenuBtn.classList.add("is-hidden");
   closeMenuBtn.classList.remove("is-hidden");
 
+  burgerMenu.style.height = "100vh";
+
   // if (localStorage.getItem(IS_CUSTOMER_LOGGED_IN)) {
-    burgerMenu.innerHTML = customerLoggedIn;
+    // burgerMenu.innerHTML = customerLoggedIn;
   //} else{
-  //     burgerMenu.innerHTML = customerNouLoggedIn;
+      // burgerMenu.innerHTML = customerNouLoggedIn;
   // }
 }
 
