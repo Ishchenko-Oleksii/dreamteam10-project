00e3a91cd9db5e542635f3284ca4ba91c641bd3d
GlobalSatUA <alex.trenbach@gmail.com>
2023-05-18 23:09:48 +0300

    fix

--- src/js/sitebar.js
+++ src/js/sitebar.js
@@ -1,33 +1,38 @@
 import axios from 'axios';
+import { fetchCategoryBooks, clearBookShell, updateBooksPerCategory, createBookElement } from './home.js';
 
-const scrollbar = document.querySelector('.scrollbar');
 const insertingCategories = document.querySelector('.aside-list');
+
 scrolling();
 fetchCategories();
+
+let selectedCategory = 'All categories'; 
 
 async function fetchCategories() {
   const apiCategories = 'https://books-backend.p.goit.global/books/category-list';
 
-  await axios.get(apiCategories).then(response => {
-    console.log(response.data);
-    let buttonId = 1;
+  try {
+    const response = await axios.get(apiCategories);
+    const categoriesData = response.data;
 
+    let buttonId = 1;
     insertingCategories.insertAdjacentHTML(
       'afterbegin',
       `<li class="category-aside-list"><button type="button" id="${buttonId}" class="aside-list-button selected-category">All categories</button></li>`
     );
 
-    for (let i = 0; i < response.data.length; i++) {
+    for (let i = 0; i < categoriesData.length; i++) {
       buttonId += 1;
-
       insertingCategories.insertAdjacentHTML(
         'beforeend',
-        `<li class="category-aside-list"><button type="button" id="btnAsideListId${buttonId}" class="aside-list-button">${response.data[i].list_name}</button></li>`
+        `<li class="category-aside-list"><button type="button" id="btnAsideListId${buttonId}" class="aside-list-button">${categoriesData[i].list_name}</button></li>`
       );
     }
 
-    attachEventListeners();
-  });
+    attachEventListeners(); 
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function scrolling() {
@@ -38,66 +43,41 @@ function scrolling() {
   });
 }
 
-let selectedCategory = 'All categories';
-let encodedString = '';
-
 function attachEventListeners() {
   const buttons = document.querySelectorAll('.aside-list-button');
-
   buttons.forEach(button => {
     button.addEventListener('click', function () {
       buttons.forEach(btn => {
         btn.classList.remove('selected-category');
       });
-
       selectedCategory = this.textContent;
       this.classList.add('selected-category');
-      encodedString = encodeURIComponent(selectedCategory);
-      if(selectedCategory!='All categories'){
-      document.querySelector('.export-all').innerHTML = '';
+
+      if (selectedCategory === 'All categories') {
+        window.location.href = 'index.html';
+      } else {
+        fetchCategoryBooks(selectedCategory)
+          .then((books) => {
+            clearBookShell();
+            updateBooksPerCategory();
+            books.forEach(book => {
+              const { book_image, title, author, _id } = book;
+              const bookElement = createBookElement(book_image, title, author, _id);
+              if (bookElement) {
+                document.querySelector('.bookShell').appendChild(bookElement);
+              }
+            });
+          })
+          .catch(error => {
+            console.error(error);
+          });
       }
-      getElementsByCategory();
     });
   });
 }
-import { setHome } from './home';
-function getElementsByCategory() {
-  document.querySelector('.export-all').innerHTML = '';
-  axios.get(`https://books-backend.p.goit.global/books/category?category=${encodedString}`).then((res) => {
-    if(selectedCategory==='All categories'){
-      setHome();
-    }else if(selectedCategory!='All categories'){
-    const books = res.data;
-    books.forEach((book) => {
-      const bookImage = book.book_image;
-      const bookTitle = book.title;
-      const bookAuthor = book.author;
-      const bookContainer = document.createElement('div');
-      bookContainer.classList.add('book-item');
-      const imgElement = document.createElement('img');
-      imgElement.src = bookImage;
-      imgElement.classList.add('insertedByCategoryBooks');
-      const titleElement = document.createElement('p');
-      titleElement.classList.add('book-title');
-      if (bookTitle.length > 15) {
-        titleElement.textContent = bookTitle.substring(0, 15) + '...';
-      } else {
-        titleElement.textContent = bookTitle;
-      }
 
-      const authorElement = document.createElement('p');
-      authorElement.classList.add('book-author');
 
-      if (bookAuthor.length > 15) {
-        authorElement.textContent = bookAuthor.substring(0, 15) + '...';
-      } else {
-        authorElement.textContent = bookAuthor;
-      }
-      bookContainer.appendChild(imgElement);
-      bookContainer.appendChild(titleElement);
-      bookContainer.appendChild(authorElement);
-      document.querySelector('.export-all').appendChild(bookContainer);
-    });
-  }
-  });
-}
+
+export { selectedCategory, attachEventListeners };
+
+
