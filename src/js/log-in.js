e537a570f8b1649f7bdde0c76f389e2038ad3817
Oleksii <ishchenkook@gmail.com>
2023-05-20 18:20:30 +0300

    fix bugs

--- src/js/log-in.js
+++ src/js/log-in.js
@@ -1,36 +1,35 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAYO4Ie3dyg57FYXwv2s34tOoBXm3UaMzg",
-    authDomain: "users-bookshelf.firebaseapp.com",
-    projectId: "users-bookshelf",
-    storageBucket: "users-bookshelf.appspot.com",
-    messagingSenderId: "1093946480659",
-    appId: "1:1093946480659:web:6fdb7ef580bf859789c7e6"
+  apiKey: 'AIzaSyAYO4Ie3dyg57FYXwv2s34tOoBXm3UaMzg',
+  authDomain: 'users-bookshelf.firebaseapp.com',
+  projectId: 'users-bookshelf',
+  storageBucket: 'users-bookshelf.appspot.com',
+  messagingSenderId: '1093946480659',
+  appId: '1:1093946480659:web:6fdb7ef580bf859789c7e6',
 };
 
 // Import the functions you need from the SDKs you need
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { modalSignUp } from "./modal-signup";
-import { initializeApp } from "firebase/app";
-
+import { modalSignUp } from './modal-signup';
+import { initializeApp } from 'firebase/app';
 
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
 } from 'firebase/auth';
 
 import {
-    collection,
-    doc,
-    getDoc,
-    getDocs,
-    getFirestore,
-    query,
-    setDoc,
-    updateDoc,
-    where
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  setDoc,
+  updateDoc,
+  where,
 } from 'firebase/firestore';
 
 const app = initializeApp(firebaseConfig);
@@ -44,14 +43,14 @@ var IS_CUSTOMER_LOGGED_IN = false;
 let CUSTOMER_SESSION_ID = '';
 const CUSTOMER_NAME = 'customer_name';
 
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        const uid = user.uid;
+onAuthStateChanged(auth, user => {
+  if (user) {
+    const uid = user.uid;
 
-        IS_CUSTOMER_LOGGED_IN = true;
-        CUSTOMER_SESSION_ID = user.uid;
-    }
-    localStorage.setItem('IS_CUSTOMER_LOGGED_IN', IS_CUSTOMER_LOGGED_IN);
+    IS_CUSTOMER_LOGGED_IN = true;
+    CUSTOMER_SESSION_ID = user.uid;
+  }
+  localStorage.setItem('IS_CUSTOMER_LOGGED_IN', IS_CUSTOMER_LOGGED_IN);
 });
 
 const signinCont = document.querySelector('.signin-cont');
@@ -64,12 +63,11 @@ modalSignUp();
 
 tabs.addEventListener('click', changeTab);
 function changeTab(event) {
+  signin.classList.toggle('active');
+  signup.classList.toggle('active');
 
-    signin.classList.toggle("active");
-    signup.classList.toggle("active");
-
-    signinCont.classList.toggle("hidden");
-    signupCont.classList.toggle("hidden");
+  signinCont.classList.toggle('hidden');
+  signupCont.classList.toggle('hidden');
 }
 
 // processing formSignUp **************************
@@ -78,31 +76,32 @@ const formSignUp = document.querySelector('.js-form-signup');
 
 formSignUp.addEventListener('submit', onSignUp);
 function onSignUp(event) {
-    event.preventDefault();
-    const { name, email, password } = event.currentTarget.elements;
+  event.preventDefault();
+  const { name, email, password } = event.currentTarget.elements;
 
-    createUserWithEmailAndPassword(auth, email.value, password.value)
-        .then(async (userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            await setDoc(doc(db, COLLECTION_CUSTOMERS, email.value), {
-                customer_name: name.value,
-                customer_email: email.value,
-                shopping_list: [],
-                customer_avatar: '',
-                session_id: user.uid
-            });
-            Notify.success('Your account was successfully registered!');
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then(async userCredential => {
+      // Signed in
+      const user = userCredential.user;
+      await setDoc(doc(db, COLLECTION_CUSTOMERS, email.value), {
+        customer_name: name.value,
+        customer_email: email.value,
+        shopping_list: [],
+        customer_avatar: '',
+        session_id: user.uid,
+      });
+      Notify.success('Your account was successfully registered!');
 
-            // Redirect to home page
-            delay(1000).then(() => document.location.href = '/dreamteam10-project/');
-        })
-        .catch((error) => {
-            const errorMessage = error.message;
-            Notify.failure(errorMessage)
-        });
+      // Redirect to home page
+      delay(1000).then(
+        () => (document.location.href = '/dreamteam10-project/')
+      );
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      Notify.failure(errorMessage);
+    });
 }
-
 
 // processing formSignIn **************************
 
@@ -110,88 +109,98 @@ const formSignIn = document.querySelector('.js-form-signin');
 formSignIn.addEventListener('submit', onSignIn);
 /**
  * Sing In function
- * 
- * @param {*} event 
+ *
+ * @param {*} event
  */
 function onSignIn(event) {
-    event.preventDefault();
-    const { email, password } = event.currentTarget.elements;
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
 
-    signInWithEmailAndPassword(auth, email.value, password.value)
-        .then(async (userCredential) => {
-            // Signed in 
-            const authUser = userCredential.user;
-            const currentUser = doc(db, COLLECTION_CUSTOMERS, email.value);
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(async userCredential => {
+      // Signed in
+      const authUser = userCredential.user;
+      const currentUser = doc(db, COLLECTION_CUSTOMERS, email.value);
 
-            // Save cutomer session ID to DB
-            await updateDoc(currentUser, { session_id: authUser.uid });
+      // Save cutomer session ID to DB
+      await updateDoc(currentUser, { session_id: authUser.uid });
 
-            // Restoring Shopping Cart list from the DB if exists
-            const currentUserDocument = await getDoc(currentUser);
-            if (currentUserDocument.exists()) {
-                localStorage.setItem(CUSTOMER_NAME, currentUserDocument.data().customer_name);
-                localStorage.setItem(LOCALSTOR_KEY, currentUserDocument.data().shopping_list);
-            }
+      // Restoring Shopping Cart list from the DB if exists
+      const currentUserDocument = await getDoc(currentUser);
+      if (currentUserDocument.exists()) {
+        localStorage.setItem(
+          CUSTOMER_NAME,
+          currentUserDocument.data().customer_name
+        );
+        localStorage.setItem(
+          LOCALSTOR_KEY,
+          currentUserDocument.data().shopping_list
+        );
+      }
 
-            // Redirect to home page
-            Notify.success('Welcome!');
-            delay(1000).then(() => document.location.href = '/dreamteam10-project/');
-        })
-        .catch((error) => {
-            const errorMessage = error.message;
-            Notify.failure(errorMessage)
-        });
+      // Redirect to home page
+      Notify.success('Welcome!');
+      delay(1000).then(
+        () => (document.location.href = '/dreamteam10-project/')
+      );
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      Notify.failure(errorMessage);
+    });
 }
 
-
 const signout = document.querySelectorAll('.js-signout');
-signout.forEach(e => e.addEventListener("click", onSignOut));
+signout.forEach(e => e.addEventListener('click', onSignOut));
 
 /**
  *  Sign Out function
  */
 function onSignOut() {
-    signOut(auth).then(async () => {
-        if (CUSTOMER_SESSION_ID == "") {
-            return true;
-        }
+  signOut(auth)
+    .then(async () => {
+      if (CUSTOMER_SESSION_ID == '') {
+        return true;
+      }
 
-        const searchBySessionIdQuery = query(
-            collection(db, COLLECTION_CUSTOMERS),
-            where("session_id", "==", CUSTOMER_SESSION_ID)
-        );
+      const searchBySessionIdQuery = query(
+        collection(db, COLLECTION_CUSTOMERS),
+        where('session_id', '==', CUSTOMER_SESSION_ID)
+      );
 
-        const queryResults = await getDocs(searchBySessionIdQuery);
+      const queryResults = await getDocs(searchBySessionIdQuery);
 
-        let customerEmail = '';
+      let customerEmail = '';
 
-        queryResults.forEach((doc) => {
-            customerEmail = doc.data().customer_email;
+      queryResults.forEach(doc => {
+        customerEmail = doc.data().customer_email;
+      });
+
+      // Save cutomer shopping list into DB during logout
+      if (customerEmail) {
+        const currentUser = doc(db, COLLECTION_CUSTOMERS, customerEmail);
+
+        await updateDoc(currentUser, {
+          shopping_list: localStorage.getItem(LOCALSTOR_KEY),
         });
+      }
+      localStorage.removeItem(CUSTOMER_NAME);
+      localStorage.removeItem(LOCALSTOR_KEY);
+      localStorage.setItem('IS_CUSTOMER_LOGGED_IN', false);
+      Notify.warning('Have a nice day!');
 
-        // Save cutomer shopping list into DB during logout
-        if (customerEmail) {
-            const currentUser = doc(db, COLLECTION_CUSTOMERS, customerEmail);
-
-            await updateDoc(currentUser, {
-                shopping_list: localStorage.getItem(LOCALSTOR_KEY)
-            });
-        }
-        localStorage.removeItem(CUSTOMER_NAME);
-        localStorage.removeItem(LOCALSTOR_KEY);
-        localStorage.setItem('IS_CUSTOMER_LOGGED_IN', false);
-        Notify.warning('Have a nice day!');
-
-        // Redirect to home page
-        delay(1000).then(() => document.location.href = '/dreamteam10-project/');
-
-    }).catch((error) => {
-        const errorMessage = error.message;
-        // An error happened.
-        Notify.failure(errorMessage)
+      // Redirect to home page
+      delay(1000).then(
+        () => (document.location.href = '/dreamteam10-project/')
+      );
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      // An error happened.
+      Notify.failure(errorMessage);
     });
 }
 
 function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise(resolve => setTimeout(resolve, time));
 }
--- src/js/modalBook.js
+++ src/js/modalBook.js
@@ -4,107 +4,108 @@ import imageBookShop from '../images/bookshop.svg';
 import imageIBooks from '../images/ibooks.svg';
 import sprite from '../images/amazon.svg';
 
-const refs={
-    div: document.querySelector('.bookShell'),
-   
-    closeModalBtn: document.querySelector('[data-action="close-modal"]'),
-    backdrop: document.querySelector('.js-backdrop'),
-    bookCard: document.querySelector('.js-book-card'),
-    shoppingListBtn: document.querySelector('[data-action="shopping-list-modal"]'),
-    text: document.querySelector('.js-modal-text'),
-}
+const refs = {
+  div: document.querySelector('.bookShell'),
+
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector('.js-backdrop'),
+  bookCard: document.querySelector('.js-book-card'),
+  shoppingListBtn: document.querySelector(
+    '[data-action="shopping-list-modal"]'
+  ),
+  text: document.querySelector('.js-modal-text'),
+};
 
 const STORAGE_KEY = 'info-shopping-list';
 
-let currentBookInfo={};
-let arrShoppingList=[];
-if (localStorage.getItem(STORAGE_KEY)){
-    arrShoppingList=JSON.parse(localStorage.getItem(STORAGE_KEY))
+let currentBookInfo = {};
+let arrShoppingList = [];
+if (localStorage.getItem(STORAGE_KEY)) {
+  arrShoppingList = JSON.parse(localStorage.getItem(STORAGE_KEY));
 }
 
-refs.div.addEventListener('click',onItemGalleryBooksClick);
+refs.div.addEventListener('click', onItemGalleryBooksClick);
 
 refs.closeModalBtn.addEventListener('click', onCloseModal);
 refs.backdrop.addEventListener('click', onBackdropClick);
 refs.shoppingListBtn.addEventListener('click', onShoppingListBtnClick);
 
-function onItemGalleryBooksClick(event){
-    event.preventDefault();
+function onItemGalleryBooksClick(event) {
+  event.preventDefault();
 
-    const isDivQuickViewEl=event.target.classList.contains('quick-view');
-    const isImageGalleryEl=event.target.classList.contains('js-gallery-image');
- 
-    if(!isImageGalleryEl && !isDivQuickViewEl){
-        return;
-    }
+  const isDivQuickViewEl = event.target.classList.contains('quick-view');
+  const isImageGalleryEl = event.target.classList.contains('js-gallery-image');
 
-    const idBook=event.target.dataset.id;
-    
-    const isBookInShoppingList=arrShoppingList.length===0
-    ?false
-    :arrShoppingList.find(book=>book.bookId===idBook);
-    
-    if(isBookInShoppingList){
-      refs.shoppingListBtn.textContent='remove from the shopping list';
-      refs.shoppingListBtn.classList.add('modal__button-shopping-list--remove');
-      refs.text.classList.remove('visually-hidden');
-    }
-    else{
-      refs.shoppingListBtn.textContent='add to shopping list';
-      refs.text.classList.add('visually-hidden');
-      refs.shoppingListBtn.classList.remove('modal__button-shopping-list--remove');
-    }
-    refs.bookCard.innerHTML='';
+  if (!isImageGalleryEl && !isDivQuickViewEl) {
+    return;
+  }
 
-    API.fetchBookById(idBook)
+  const idBook = event.target.dataset.id;
+
+  const isBookInShoppingList =
+    arrShoppingList.length === 0
+      ? false
+      : arrShoppingList.find(book => book.bookId === idBook);
+
+  if (isBookInShoppingList) {
+    refs.shoppingListBtn.textContent = 'remove from the shopping list';
+    refs.shoppingListBtn.classList.add('modal__button-shopping-list--remove');
+    refs.text.classList.remove('visually-hidden');
+  } else {
+    refs.shoppingListBtn.textContent = 'add to shopping list';
+    refs.text.classList.add('visually-hidden');
+    refs.shoppingListBtn.classList.remove(
+      'modal__button-shopping-list--remove'
+    );
+  }
+  refs.bookCard.innerHTML = '';
+
+  API.fetchBookById(idBook)
     .then(createMarkupBookModal)
-    .catch(err=>console.err('Ooops! Something went wrong!'));
+    .catch(err => console.err('Ooops! Something went wrong!'));
 
-    onOpenModal();
-       
+  onOpenModal();
 }
 
 function onOpenModal() {
-    window.addEventListener('keydown', onEscKeyPress);
-    document.body.classList.add('js-show-modal');
-
+  window.addEventListener('keydown', onEscKeyPress);
+  document.body.classList.add('js-show-modal');
 }
-  
+
 function onCloseModal() {
-    window.removeEventListener('keydown', onEscKeyPress);
-    document.body.classList.remove('js-show-modal');
+  window.removeEventListener('keydown', onEscKeyPress);
+  document.body.classList.remove('js-show-modal');
 }
 
 function onBackdropClick(event) {
-    if (event.currentTarget === event.target) {
-        onCloseModal();
-    }
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
 }
 
 function onEscKeyPress(event) {
-    const ESC_KEY_CODE = 'Escape';
-    const isEscKey = event.code === ESC_KEY_CODE;
-    if (isEscKey) {
-        onCloseModal();
-    }
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+  if (isEscKey) {
+    onCloseModal();
+  }
 }
 
+function createMarkupBookModal(arrayInfoBook) {
+  const { book_image, author, title, description, buy_links, _id, list_name } =
+    arrayInfoBook;
 
-function createMarkupBookModal(arrayInfoBook){
-    const {book_image, author, title, description,buy_links, _id, list_name}=arrayInfoBook;
+  currentBookInfo.bookId = _id;
+  currentBookInfo.bookAuthor = author;
+  currentBookInfo.bookTitle = title;
+  currentBookInfo.bookDescription = description;
+  currentBookInfo.bookCategory = list_name;
+  currentBookInfo.bookImage = book_image;
+  currentBookInfo.bookAmazon = buy_links[0].url;
+  currentBookInfo.bookOpenBook = buy_links[1].url;
+  currentBookInfo.bookShop = buy_links[4].url;
 
-    currentBookInfo.bookId=_id;
-    currentBookInfo.bookAuthor=author;
-    currentBookInfo.bookTitle=title;
-    currentBookInfo.bookDescription=description;
-    currentBookInfo.bookCategory=list_name;
-    currentBookInfo.bookImage=book_image;
-    currentBookInfo.bookAmazon=buy_links[0].url;
-    currentBookInfo.bookOpenBook=buy_links[1].url;
-    currentBookInfo.bookShop=buy_links[4].url;
-
-    const markup=
-    `<img class="book-image"" src="${book_image}" alt="${title}" loading="lazy" data-id="${_id}"/>
+  const markup = `<img class="book-image"" src="${book_image}" alt="${title}" loading="lazy" data-id="${_id}"/>
     <div class="book-description-thumb">
     <p class="book-title">${title}</p>
     <p class="book-author">${author}</p>
@@ -123,27 +124,26 @@ function createMarkupBookModal(arrayInfoBook){
     </a>
     </div>
     </div>`;
-    
-    refs.bookCard.innerHTML=markup;
+
+  refs.bookCard.innerHTML = markup;
 }
 
+function onShoppingListBtnClick(event) {
+  try {
+    const indexBook = arrShoppingList.findIndex(
+      book => book.bookId === currentBookInfo.bookId
+    );
 
-function onShoppingListBtnClick(event){  
-    try{
-        const indexBook=arrShoppingList.findIndex(book=>book.bookId===currentBookInfo.bookId);
-
-        if(indexBook!==-1){
-            arrShoppingList.splice(indexBook,1);
-            localStorage.removeItem(STORAGE_KEY);
-            localStorage.setItem(STORAGE_KEY, JSON.stringify(arrShoppingList));
-        }
-        else{
-            arrShoppingList.push({...currentBookInfo});
-            localStorage.setItem(STORAGE_KEY, JSON.stringify(arrShoppingList));
-        }   
+    if (indexBook !== -1) {
+      arrShoppingList.splice(indexBook, 1);
+      localStorage.removeItem(STORAGE_KEY);
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(arrShoppingList));
+    } else {
+      arrShoppingList.push({ ...currentBookInfo });
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(arrShoppingList));
     }
-    catch{
-        console.err('Ooops! Something went wrong!');
-    }
-    onCloseModal();
+  } catch {
+    console.err('Ooops! Something went wrong!');
+  }
+  onCloseModal();
 }
